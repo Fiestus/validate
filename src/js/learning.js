3d58836623ffad060f850bf9bc4587dc76ff8d3d
Fiestus <goblik88@mail.ru>
2020-05-11 15:04:41 +0600

    3 задание

--- src/js/learning.js
+++ src/js/learning.js
@@ -4,15 +4,12 @@ let money, time;
 
 function start() {
     time = prompt('Введите дату в формате YYYY-MM-DD', '');
-    
+
     while (isNaN(money) || money === '' || money === null || money === 0) {
         money = +prompt('Ваш бюджет на месяц?', '');
     }
 }
 start();
-
-
-
 
 let appData = {
     moneyData: money,
@@ -38,20 +35,32 @@ function calculateExpenses() {
 }
 calculateExpenses();
 
-appData.moneyPerDay = (appData.moneyData / 30).toFixed();
-
-alert('Ежедневный бюджет: ' + appData.moneyPerDay);
-
-if (appData.moneyData < 200) {
-    console.log('Минимальный уровень достатка');
-} else if (appData.moneyData > 200 && appData.moneyData < 2000) {
-    console.log('Средний уровень достатка');
-} else if (appData.moneyData > 2000) {
-    console.log('Высокий уровень достатка');
-} else {
-    console.log('Произошла ошибка');
+function chooseOptExpenses() {
+    for (let i = 0; i < 3; i++) {
+        let ansver = prompt('Статья необязательных расходов?', '');
+        appData.optionalExpenses[i + 1] = ansver;
+    }
 }
+chooseOptExpenses();
 
+function detectDayBudget() {
+    appData.moneyPerDay = (appData.moneyData / 30).toFixed();
+    alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+}
+detectDayBudget();
+
+function detectLevel() {
+    if (appData.moneyData < 200) {
+        console.log('Минимальный уровень достатка');
+    } else if (appData.moneyData > 200 && appData.moneyData < 2000) {
+        console.log('Средний уровень достатка');
+    } else if (appData.moneyData > 2000) {
+        console.log('Высокий уровень достатка');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
+detectLevel();
 
 function calculateSavings() {
 
@@ -82,3 +91,49 @@ console.log(appData);
 */
 
 ///////////////////////////////////////
+
+
+// function slow() {
+//     setTimeout(function () {
+//         console.log('I\'m slow function');
+//     }, 1000);
+// }
+//
+// function fast() {
+//     console.log("I'm fast function");
+// }
+//
+//
+// function callback(callback) {
+//     setTimeout(function () {
+//         console.log(`I'm callback function`);
+//         // console.log('I\'m slow function');
+//     }, 1000);
+//
+//     callback();
+// }
+//
+// callback(slow, fast);
+
+
+//
+// slow();
+// fast();
+
+////////////////////
+
+// let options = {
+//   width: 1024,
+//   height: 768
+// };
+//
+// options.isRetina = false;
+//
+// options.colors = {
+//     red: 'red',
+//     black: 'black'
+// };
+//
+// delete options.isRetina;
+//
+// console.log(Object.keys(options).length);
